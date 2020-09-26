7f43938130256e5db90a81eae683c7871a138fbd
blueju <blueju@foxmail.com>
2020-09-26 15:05:24 +0800

    子应用切换

--- main-app/src/layouts/MicroAppLayout.tsx
+++ main-app/src/layouts/MicroAppLayout.tsx
@@ -1,8 +1,41 @@
+// 实现方式一
 import { MicroApp } from 'umi';
 import React from 'react';
 
-function MicroAppLayout() {
-  return <MicroApp name="sub-app-1" />;
+class MicroAppLayout extends React.Component {
+  state = {
+    name: '',
+  };
+
+  componentWillMount() {
+    const name = location.pathname.slice(1);
+    this.setState({
+      name,
+    });
+  }
+
+  render() {
+    return <MicroApp name={this.state.name} />;
+  }
 }
 
 export default MicroAppLayout;
+
+// 实现方式二
+// import { MicroApp } from 'umi';
+// import React from 'react';
+
+// let name = '';
+
+// class MicroAppLayout extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     name = location.pathname.slice(1);
+//   }
+
+//   render() {
+//     return <MicroApp name={name} />;
+//   }
+// }
+
+// export default MicroAppLayout;
