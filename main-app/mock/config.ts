d24ecd2187ad21d8650eb3777a62db4ea82deada
blueju <blueju@foxmail.com>
2020-09-26 14:57:35 +0800

    修改子应用Mock请求配置与子应用路由Mock请求配置

--- main-app/mock/config.ts
+++ main-app/mock/config.ts
@@ -5,11 +5,19 @@ export default {
         name: 'sub-app-1',
         entry: '//localhost:8001',
       },
+      {
+        name: 'sub-app-2',
+        entry: '//localhost:8002',
+      },
     ],
     routes: [
       {
         name: 'sub-app-1',
         path: '/sub-app-1',
+      },
+      {
+        name: 'sub-app-2',
+        path: '/sub-app-2',
       },
     ],
   },
